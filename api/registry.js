66f6db831cbe194727a34cc025820a735a8d2aa6
Daniele Ugolini <dan.ugolini@gmail.com>
2020-10-23 23:09:00 +0200

    added dpi

--- api/registry.js
+++ api/registry.js
@@ -24,6 +24,7 @@ module.exports = {
     uniAddress: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
     snxAddress: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',
     wbtcAddress: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    dpicAddress: '0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b',
 
     tokens: [
         '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', //wethAddress, 
@@ -33,7 +34,8 @@ module.exports = {
         '0xd533a949740bb3306d119cc777fa900ba034cd52', //crvAddress 
         '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', //uniAddress 
         '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //snxAddress 
-        '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' //wbtcAddress 
+        '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', //wbtcAddress 
+        '0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b' //dpicAddress 
     ],
 
 
